6282e0d994f979d9f9d1eb28ba5bb1ba5def21a3
LuettgeM <98285593+LuettgeM@users.noreply.github.com>
2023-01-02 22:45:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,12 @@ def get_fruityvice_data(fruit_choice):
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
+def get_fruit_load_list(my_cnx):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * FROM fruit_load_list")
+        return my_cur.fetchall()
+    
+
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),["Avocado","Strawberries"])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
@@ -38,9 +44,8 @@ fruityvice_normalized = get_fruityvice_data(fruit_choice)
 streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM fruit_load_list")
-my_data_rows = my_cur.fetchall()
+my_data_rows = get_fruit_load_list(my_cnx)
+
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
